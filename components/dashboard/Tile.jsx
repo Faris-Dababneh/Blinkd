5977bd77d980167ae3c4ed2ebcca68047f04c671
Faris <datiger365@gmail.com>
2024-09-28 20:48:11 -0400

    Added modal in dashboard for changing answers

--- components/dashboard/Tile.jsx
+++ components/dashboard/Tile.jsx
@@ -1,20 +1,48 @@
-import React from 'react'
+'use client'
+// IMPROVE ANSWER MODAL APPEARANCE AND IMPLEMENT SAVE FEATURE FOR ANSWER CHANGING
+import React, {useState} from 'react'
+import { useDisclosure } from '@mantine/hooks';
+import { Button } from '@nextui-org/react';
+import { Duration } from './questions/Duration';
+import { Interests } from './questions/Interests';
+import { Modal } from '@mantine/core';
+import { MantineProvider } from '@mantine/core';
 
 export const Tile = ({answerName, answer, session}) => {
+  const [opened, handlers] = useDisclosure(false);
+  const [selectedAnswer, setSelectedAnswer] = useState();
+
+  const openAnswer = (event) => {
+    switch (answerName) {
+      case 'duration':
+        setSelectedAnswer(<Duration />);
+        handlers.open();
+        break;
+      case 'interests':
+        setSelectedAnswer(<Interests />);
+        handlers.open();
+    }
+  }
 
   return (
-    <div className='rounded-lg border w-64 h-64 p-10'>
-      <h1 className='text-lg font-semibold underline uppercase'>{answerName}</h1>
-      {answer.length > 1 ? (
-        <div>
-          {answer.map((item) => (
-            <>{item !== answerName ? <p>{item}</p> : <p></p>}</>
-          ))}
-        </div>
-      ) : (
-        <></>
-      )}
-      
-    </div>
+    <MantineProvider>
+      <div className='flex flex-col rounded-lg border w-64 h-64 p-10'>
+        <h1 className='text-lg font-semibold underline uppercase'>{answerName}</h1>
+        {answer.length > 1 ? (
+          <div>
+            {answer.map((item) => (
+              <>{item !== answerName ? <p>{item}</p> : <p></p>}</>
+            ))}
+          </div>
+        ) : (
+          <></>
+        )}
+        <Button className='mt-auto self-start w-full bg-primary text-white py-2 px-4 rounded-full' onPress={(event) => openAnswer(event)}>Change Answer</Button>
+        <Modal opened={opened} onClose={() => handlers.close()} centered size={'xl'}>
+          {selectedAnswer}
+        </Modal>
+    
+      </div>
+    </MantineProvider>
   )
 }
---
+++ components/dashboard/questions/Interests.jsx
@@ -0,0 +1,36 @@
+import React, {useState} from 'react'
+import {DateRangePicker} from "@nextui-org/react";
+import Cookies from 'js-cookie'
+import {getLocalTimeZone, today, parseDate} from "@internationalized/date";
+import { MantineProvider } from '@mantine/core';
+import { TagsInput } from '@mantine/core';
+
+export const Interests = () => {
+    let interestsCookie = Cookies.get('interests') ? JSON.parse(Cookies.get('interests')) : null;
+    const [interests, setInterests] = useState(interestsCookie ? interestsCookie : []);
+
+    const interestsChange = (event) => {
+        setInterests(event)
+        console.log(event)
+        Cookies.set('interests', `${JSON.stringify(event)}`)
+        console.log(JSON.parse(Cookies.get('interests')))
+    }
+
+    return (
+        <MantineProvider>
+          <div className='flex justify-center items-center w-full flex-col overflow-hidden px-4'>
+            <h1 className='text-black text-center text-2xl sm:text-4xl font-semibold'>What news categories are you interested in?</h1>
+            <p className='py-3 text-center'>This will help personalize your feed for your liking.</p>
+            <TagsInput
+              label="Your interests"
+              placeholder="Enter interests here"
+              data={['Science', 'Technology', 'Politics', 'World', 'Business', 'Entertainment', 'Health', 'Fitness', 'Environment', 'Sports', 'Art', 'Culture']}
+              value={interests}
+              onChange={(event) => interestsChange(event)}
+              clearable
+              className='w-3/4 sm:w-1/2 lg:w-1/3'
+            />
+          </div>
+        </MantineProvider>
+    );
+}
